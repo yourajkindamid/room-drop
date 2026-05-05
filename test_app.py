5992863c6c46a8254eeebf441f28422397e01f87
Abdul Raffay Gill <abdulraffaykhadimgill@gmail.com>
2026-05-05 13:52:43 +0500

    initialization of R2 client for remote uploading in cloudflare R2 bucket

--- test_app.py
+++ test_app.py
@@ -8,6 +8,17 @@ DATABASE_URL = "postgresql://postgres.fzuoyaxlwwsanlsicmoo:hosh_me_aao_abhijeet_
 app = Flask(__name__)
 app.secret_key = 'diddy_blud_managment_system'
 
+def get_r2_client():
+    account_id = os.getenv('R2_ACCOUNT_ID')
+    endpoint_url = f"https://{account_id}.r2.cloudflarestorage.com"
+    return boto3.client(
+        service_name='s3',
+        endpoint_url=endpoint_url,
+        aws_access_key_id=os.getenv('R2_ACCESS_KEY'),
+        aws_secret_access_key=os.getenv('R2_SECRET_KEY'),
+        region_name='auto',
+    )
+
 @app.route('/', methods=['GET'])
 def home_page():
     return render_template('index.html')
