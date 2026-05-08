4a6fc3c64d3af291c55c32bb1ae08626522bb881
Abdul Raffay Gill <abdulraffaykhadimgill@gmail.com>
2026-05-08 23:31:15 +0500

    finalize codebase

--- test_app.py
+++ test_app.py
@@ -1259,4 +1259,5 @@ if __name__ == '__main__':
     if not app.debug or os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
         start_scheduler()
 
-    socketio.run(app, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=False)
